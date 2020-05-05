5b33125ee6487808e533267c93d6042155e95d4f
Jakub D <mrbestkubek@gmail.com>
2020-05-05 17:05:30 +0200

    add project

---
+++ h2db/SpringbootH2Database/src/main/java/com/springboot/h2/Appmain.java
@@ -0,0 +1,13 @@
+package com.springboot.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
+@SpringBootApplication
+public class Appmain {
+	public static void main(String[] args) {
+		SpringApplication.run(Appmain.class, args);
+	}
+}
---
+++ h2db/SpringbootH2Database/src/main/java/com/springboot/h2/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.h2.repository;
+
+import com.springboot.h2.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Integer> {
+
+}
